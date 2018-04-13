98f4070c6d55f44bf1b8db9d82ba1eca9ba91ff1
kayson0512 <kayson0512@163.com>
2018-04-13 09:41:27 +0800

    login finished

--- src/page/login.vue
+++ src/page/login.vue
@@ -1,139 +1,162 @@
 <template>
-  	<div class="login_page fillcontain">
-	  	<transition name="form-fade" mode="in-out">
-	  		<section class="form_contianer" v-show="showLogin">
-		  		<div class="manage_tip">
-		  			<p>elm后台管理系统</p>
-		  		</div>
-		    	<el-form :model="loginForm" :rules="rules" ref="loginForm">
-					<el-form-item prop="username">
-						<el-input v-model="loginForm.username" placeholder="用户名"><span>dsfsf</span></el-input>
-					</el-form-item>
-					<el-form-item prop="password">
-						<el-input type="password" placeholder="密码" v-model="loginForm.password"></el-input>
-					</el-form-item>
-					<el-form-item>
-				    	<el-button type="primary" @click="submitForm('loginForm')" class="submit_btn">登陆</el-button>
-				  	</el-form-item>
-				</el-form>
-				<p class="tip">温馨提示：</p>
-				<p class="tip">未登录过的新用户，自动注册</p>
-				<p class="tip">注册过的用户可凭账号密码登录</p>
-	  		</section>
-	  	</transition>
-  	</div>
+    <div class="login_page fillcontain">
+        <transition name="form-fade" mode="in-out">
+            <section class="form_contianer" v-show="showLogin">
+                <div class="manage_tip">
+                    <p>管道工程管理系统V1.0</p>
+                </div>
+                <el-form :model="loginForm" :rules="rules" ref="loginForm">
+                    <el-form-item label="用户名" prop="username">
+                        <el-input v-model="loginForm.username" placeholder="请输入您的用户名"></el-input>
+                    </el-form-item>
+                    <el-form-item label="密码" prop="password">
+                        <el-input type="password" placeholder="请输入您的密码" v-model="loginForm.password"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" @click="submitForm('loginForm')" class="submit_btn">登陆</el-button>
+                    </el-form-item>
+                </el-form>
+                <p class="tip">温馨提示：</p>
+                <p class="tip">未登录过的新用户，自动注册</p>
+                <p class="tip">注册过的用户可凭账号密码登录</p>
+            </section>
+        </transition>
+    </div>
 </template>
 
 <script>
-	import {login, getAdminInfo} from '@/api/getData'
-	import {mapActions, mapState} from 'vuex'
+import { login, getAdminInfo } from "@/api/getData";
+import { mapActions, mapState } from "vuex";
 
-	export default {
-	    data(){
-			return {
-				loginForm: {
-					username: '',
-					password: '',
-				},
-				rules: {
-					username: [
-			            { required: true, message: '请输入用户名', trigger: 'blur' },
-			        ],
-					password: [
-						{ required: true, message: '请输入密码', trigger: 'blur' }
-					],
-				},
-				showLogin: false,
-			}
-		},
-		mounted(){
-			this.showLogin = true;
-			if (!this.adminInfo.id) {
-    			this.getAdminData()
-    		}
-		},
-		computed: {
-			...mapState(['adminInfo']),
-		},
-		methods: {
-			...mapActions(['getAdminData']),
-			async submitForm(formName) {
-				this.$refs[formName].validate(async (valid) => {
-					if (valid) {
-						const res = await login({user_name: this.loginForm.username, password: this.loginForm.password})
-						if (res.status == 1) {
-							this.$message({
-		                        type: 'success',
-		                        message: '登录成功'
-		                    });
-							this.$router.push('manage')
-						}else{
-							this.$message({
-		                        type: 'error',
-		                        message: res.message
-		                    });
-						}
-					} else {
-						this.$notify.error({
-							title: '错误',
-							message: '请输入正确的用户名密码',
-							offset: 100
-						});
-						return false;
-					}
-				});
-			},
-		},
-		watch: {
-			adminInfo: function (newValue){
-				if (newValue.id) {
-					this.$message({
-                        type: 'success',
-                        message: '检测到您之前登录过，将自动登录'
-                    });
-					this.$router.push('manage')
-				}
-			}
-		}
-	}
+export default {
+  data() {
+    return {
+      loginForm: {
+        username: "",
+        password: ""
+      },
+      rules: {
+        username: [
+          {
+            required: true,
+            message: "请输入用户名",
+            trigger: "blur"
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: "请输入密码",
+            trigger: "blur"
+          }
+        ]
+      },
+      showLogin: false
+    };
+  },
+  mounted() {
+    this.showLogin = true;
+    if (!this.adminInfo.id) {
+      this.getAdminData();
+    }
+  },
+  computed: {
+    //mapState通过扩展运算符将store.state.orderList 映射this.orderList  这个this很重要，这个映射直接映射到当前Vue的this对象上。
+    //所以通过this都能将这些对象点出来，同理，mapActions, mapMutations都是一样的道理。若有多个，则['XXX1','XXX2']
+    ...mapState(["adminInfo"])
+  },
+  methods: {
+    ...mapActions(["getAdminData"]),
+    async submitForm(formName) {
+      this.$refs[formName].validate(async valid => {
+        if (valid) {
+          const res = await login({
+            user_name: this.loginForm.username,
+            password: this.loginForm.password
+          });
+          if (res.status == 1) {
+            this.$message({
+              type: "success",
+              message: "登录成功"
+            });
+            //路由跳转至/manage
+            this.$router.push("manage");
+          } else {
+            this.$message({
+              type: "error",
+              message: res.message
+            });
+          }
+        } else {
+          this.$notify.error({
+            title: "错误",
+            message: "请输入正确的用户名密码",
+            offset: 100
+          });
+          return false;
+        }
+      });
+    }
+  },
+  watch: {
+    adminInfo: function(newValue) {
+      if (newValue.id) {
+        this.$message({
+          type: "success",
+          message: "检测到您之前登录过，将自动登录"
+        });
+        setTimeout(() => {
+          this.$router.push("manage");
+        }, 2000);
+      }
+    }
+  }
+};
 </script>
 
 <style lang="less" scoped>
-	@import '../style/mixin';
-	.login_page{
-		background-color: #324057;
-	}
-	.manage_tip{
-		position: absolute;
-		width: 100%;
-		top: -100px;
-		left: 0;
-		p{
-			font-size: 34px;
-			color: #fff;
-		}
-	}
-	.form_contianer{
-		.wh(320px, 210px);
-		.ctp(320px, 210px);
-		padding: 25px;
-		border-radius: 5px;
-		text-align: center;
-		background-color: #fff;
-		.submit_btn{
-			width: 100%;
-			font-size: 16px;
-		}
-	}
-	.tip{
-		font-size: 12px;
-		color: red;
-	}
-	.form-fade-enter-active, .form-fade-leave-active {
-	  	transition: all 1s;
-	}
-	.form-fade-enter, .form-fade-leave-active {
-	  	transform: translate3d(0, -50px, 0);
-	  	opacity: 0;
-	}
+@import "../style/mixin";
+.login_page {
+  background-color: #324057;
+}
+
+.manage_tip {
+  position: absolute;
+  width: 100%;
+  top: -100px;
+  left: 0;
+  p {
+    font-size: 34px;
+    color: #fff;
+  }
+}
+
+.form_contianer {
+  .wh(320px, 280px);
+  .ctp(320px, 280px);
+  padding: 25px;
+  border-radius: 5px;
+  text-align: center;
+  background-color: #fff;
+  .submit_btn {
+    width: 100%;
+    font-size: 16px;
+  }
+}
+
+.tip {
+  font-size: 12px;
+  color: red;
+}
+
+.form-fade-enter-active,
+.form-fade-leave-active {
+  transition: all 1s;
+}
+
+.form-fade-enter,
+.form-fade-leave-active {
+  transform: translate3d(0, -50px, 0);
+  opacity: 0;
+}
 </style>
--- src/page/manage.vue
+++ src/page/manage.vue
@@ -1,66 +1,90 @@
 <template>
-	<div class="manage_page fillcontain">
-		<el-row style="height: 100%;">
-	  		<el-col :span="4"  style="min-height: 100%; background-color: #324057;">
-				<el-menu :default-active="defaultActive" style="min-height: 100%;" theme="dark" router>
-					<el-menu-item index="manage"><i class="el-icon-menu"></i>首页</el-menu-item>
-					<el-submenu index="2">
-						<template slot="title"><i class="el-icon-document"></i>数据管理</template>
-						<el-menu-item index="userList">用户列表</el-menu-item>
-						<el-menu-item index="shopList">商家列表</el-menu-item>
-						<el-menu-item index="foodList">食品列表</el-menu-item>
-						<el-menu-item index="orderList">订单列表</el-menu-item>
-						<el-menu-item index="adminList">管理员列表</el-menu-item>
-					</el-submenu>
-					<el-submenu index="3">
-						<template slot="title"><i class="el-icon-plus"></i>添加数据</template>
-						<el-menu-item index="addShop">添加商铺</el-menu-item>
-						<el-menu-item index="addGoods">添加商品</el-menu-item>
-					</el-submenu>
-					<el-submenu index="4">
-						<template slot="title"><i class="el-icon-star-on"></i>图表</template>
-						<el-menu-item index="visitor">用户分布</el-menu-item>
-						<!-- <el-menu-item index="newMember">用户数据</el-menu-item> -->
-					</el-submenu>
-					<el-submenu index="5">
-						<template slot="title"><i class="el-icon-edit"></i>编辑</template>
-						<!-- <el-menu-item index="uploadImg">上传图片</el-menu-item> -->
-						<el-menu-item index="vueEdit">文本编辑</el-menu-item>
-					</el-submenu>
-					<el-submenu index="6">
-						<template slot="title"><i class="el-icon-setting"></i>设置</template>
-						<el-menu-item index="adminSet">管理员设置</el-menu-item>
-						<!-- <el-menu-item index="sendMessage">发送通知</el-menu-item> -->
-					</el-submenu>
-					<el-submenu index="7">
-						<template slot="title"><i class="el-icon-warning"></i>说明</template>
-						<el-menu-item index="explain">说明</el-menu-item>
-					</el-submenu>
-				</el-menu>
-			</el-col>
-			<el-col :span="20" style="height: 100%;overflow: auto;">
-				<keep-alive>
-				    <router-view></router-view>
-				</keep-alive>
-			</el-col>
-		</el-row>
-  	</div>
+    <div class="manage_page fillcontain">
+        <el-row style="height: 100%;">
+            <el-col :span="4" style="min-height: 100%; background-color: #324057;">
+                <el-menu :default-active="defaultActive" style="min-height: 100%;" theme="dark" router>
+                    <el-menu-item index="manage">
+                        <i class="el-icon-menu"></i>首页</el-menu-item>
+                    <el-submenu index="2">
+                        <template slot="title">
+                            <i class="el-icon-document"></i>数据管理</template>
+                        <el-menu-item index="userList">用户列表</el-menu-item>
+                        <el-menu-item index="shopList">商家列表</el-menu-item>
+                        <el-menu-item index="foodList">食品列表</el-menu-item>
+                        <el-menu-item index="orderList">订单列表</el-menu-item>
+                        <el-menu-item index="adminList">管理员列表</el-menu-item>
+                    </el-submenu>
+                    <el-submenu index="3">
+                        <template slot="title">
+                            <i class="el-icon-plus"></i>添加数据</template>
+                        <el-menu-item index="addShop">添加商铺</el-menu-item>
+                        <el-menu-item index="addGoods">添加商品</el-menu-item>
+                    </el-submenu>
+                    <el-submenu index="4">
+                        <template slot="title">
+                            <i class="el-icon-star-on"></i>图表</template>
+                        <el-menu-item index="visitor">用户分布</el-menu-item>
+                        <!-- <el-menu-item index="newMember">用户数据</el-menu-item> -->
+                    </el-submenu>
+                    <el-submenu index="5">
+                        <template slot="title">
+                            <i class="el-icon-edit"></i>编辑</template>
+                        <!-- <el-menu-item index="uploadImg">上传图片</el-menu-item> -->
+                        <el-menu-item index="vueEdit">文本编辑</el-menu-item>
+                    </el-submenu>
+                    <el-submenu index="6">
+                        <template slot="title">
+                            <i class="el-icon-setting"></i>设置</template>
+                        <el-menu-item index="adminSet">管理员设置</el-menu-item>
+                        <!-- <el-menu-item index="sendMessage">发送通知</el-menu-item> -->
+                    </el-submenu>
+                    <el-submenu index="7">
+                        <template slot="title">
+                            <i class="el-icon-warning"></i>说明</template>
+                        <el-menu-item index="explain">说明</el-menu-item>
+                    </el-submenu>
+                </el-menu>
+            </el-col>
+            <el-col :span="20" style="height: 100%;overflow: auto;">
+                <!-- 在组件切换过程中将状态保留在内存中，防止重复渲染DOM。和 <transition> 相似，<keep-alive> 是一个抽象组件：它自身不会渲染一个 DOM 元素，也不会出现在父组件链中。 -->
+                <keep-alive>
+                    <router-view></router-view>
+                </keep-alive>
+            </el-col>
+        </el-row>
+    </div>
 </template>
 
 <script>
-    export default {
-		computed: {
-			defaultActive: function(){
-				return this.$route.path.replace('/', '');
-			}
-		},
+export default {
+  //   data() {
+  //     return {
+  //       isCollapse: true
+  //     };
+  //   },
+  //   methods: {
+  //     handleOpen(key, keyPath) {
+  //       console.log(key, keyPath);
+  //     },
+  //     handleClose(key, keyPath) {
+  //       console.log(key, keyPath);
+  //     }
+  //   },
+  computed: {
+    defaultActive: function() {
+      return this.$route.path.replace("/", "");
     }
+  }
+};
 </script>
 
 
 <style lang="less" scoped>
-	@import '../style/mixin';
-	.manage_page{
-		
-	}
+@import "../style/mixin";
+
+
+// .el-menu-vertical-demo:not(.el-menu--collapse) {
+//   width: 200px;
+//   min-height: 400px;
+// }
 </style>
